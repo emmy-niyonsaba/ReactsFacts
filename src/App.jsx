6f14c69bd2e4290a0894b75b2accb24102751385
Emmanuel Niyonsaba <emmanuelniyonsaba40@gmail.com>
2026-02-10 10:31:04 +0200

    fixing the ReactsFact

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+import './App.css'
+import Navbar from './components/Navbar'
+import Main from './components/Main'
+
+function App() {
+
+
+  return (
+    <>
+        <Navbar/>
+        <Main/>
+  
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+
+
+
+
+import React  from "react"
+import reactLogo from '../assets/react.svg';
+
+export default function Navbar() {
+    return (
+        <header>
+            <nav>
+            <img src={reactLogo} alt="React logo" />
+                <span>ReactFacts</span>
+            </nav>
+        </header>
+    )
+}
